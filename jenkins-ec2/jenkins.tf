8cb1672fcf467fcac4c97acdc8b52e46e32358d8
Deepika Gautam <deepika@aplimasolutions.com>
2020-12-07 20:12:58 -0800

    update interpolation expression

--- jenkins-ec2/jenkins.tf
+++ jenkins-ec2/jenkins.tf
@@ -54,5 +54,5 @@ data "aws_ami" "ubuntu" {
 }
 
 output "jenkins_endpoint" {
-  value = "${formatlist("http://%s:%s/", aws_instance.JenkinsEC2.*.public_ip, "8080")}"
+  value = formatlist("http://%s:%s/", aws_instance.JenkinsEC2.*.public_ip, "8080")
 }
